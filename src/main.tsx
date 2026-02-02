445f8b3cc1dbc8e51cfab5c6f35c7373822990b7
Saikumar-Bali <saikumarbali55@example.com>
2026-02-02 15:26:23 +0530

    Fix router + GitHub Pages basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './style.css'
-import App from './App.tsx'
+import App from './App'
 
-console.log('App loading...');
+console.log('App loading...')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/cine_verse">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
