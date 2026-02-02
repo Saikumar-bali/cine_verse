66b0164bd324b931ea159697cc169cfcad161a68
Saikumar-Bali <saikumarbali55@example.com>
2026-02-02 15:41:33 +0530

    Fix double BrowserRouter issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import Home from './pages/Home';
-import Movies from './pages/Movies';
-import Series from './pages/Series';
-import MovieDetail from './pages/MovieDetail';
-import SeriesDetail from './pages/SeriesDetail';
-import Search from './pages/Search';
-import Navbar from './components/Navbar';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import Series from './pages/Series'
+import MovieDetail from './pages/MovieDetail'
+import SeriesDetail from './pages/SeriesDetail'
+import Search from './pages/Search'
+import Navbar from './components/Navbar'
+import './App.css'
 
 interface PageProps {
-    title: string;
-    element: React.ReactNode;
+    title: string
+    element: React.ReactNode
 }
 
 function Page({ title, element }: PageProps) {
     useEffect(() => {
-        document.title = `${title} | CineVerse`;
-    }, [title]);
+        document.title = `${title} | CineVerse`
+    }, [title])
 
-    return <>{element}</>;
+    return <>{element}</>
 }
 
 function App() {
-    console.log('App component rendering...');
-    
+    console.log('App component rendering...')
+
     return (
-        <BrowserRouter basename="/cine_verse">
-            <div className="app">
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Page title="Home" element={<Home />} />} />
-                    <Route path="/movies" element={<Page title="Movies" element={<Movies />} />} />
-                    <Route path="/series" element={<Page title="Series" element={<Series />} />} />
-                    <Route path="/movie/:id" element={<Page title="Movie Detail" element={<MovieDetail />} />} />
-                    <Route path="/series/:id" element={<Page title="Series Detail" element={<SeriesDetail />} />} />
-                    <Route path="/search" element={<Page title="Search" element={<Search />} />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+        <div className="app">
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Page title="Home" element={<Home />} />} />
+                <Route path="/movies" element={<Page title="Movies" element={<Movies />} />} />
+                <Route path="/series" element={<Page title="Series" element={<Series />} />} />
+                <Route path="/movie/:id" element={<Page title="Movie Detail" element={<MovieDetail />} />} />
+                <Route path="/series/:id" element={<Page title="Series Detail" element={<SeriesDetail />} />} />
+                <Route path="/search" element={<Page title="Search" element={<Search />} />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './style.css'
 import App from './App'
-
-console.log('App loading...')
+import './style.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
